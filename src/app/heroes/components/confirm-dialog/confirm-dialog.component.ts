21a654a93f0e364cba8c1ab6135dc4a2bca8838d
AngelIpina12 <angelipina12@hotmail.com>
2024-10-10 12:53:06 -0600

    Creación de delete con advertencia en dialog

---
+++ src/app/heroes/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styles: ``
+})
+export class ConfirmDialogComponent {
+  readonly dialogRef = inject(MatDialogRef<ConfirmDialogComponent>);
+  readonly data = inject(MAT_DIALOG_DATA);
+
+  onNoClick():void{
+    this.dialogRef.close(false);
+  }
+
+  onConfirm():void{
+    this.dialogRef.close(true)
+  }
+}

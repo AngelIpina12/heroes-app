563a822a1d46027f927ec6af22752ebc23706e4b
AngelIpina12 <angelipina12@hotmail.com>
2024-10-10 21:58:13 -0600

    Creación de método de checkAuthentication

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -18,9 +18,25 @@ export class AuthService {
 
   login(email: string, password: string):Observable<User>{
     return this.httpClient.get<User>(`${this.baseUrl}/users/1`)
-    .pipe(
-      tap(user => this.user = user),
-      tap(user => localStorage.setItem('token', user.id.toString())),
-    )
+      .pipe(
+        tap(user => this.user = user),
+        tap(user => localStorage.setItem('token', user.id.toString())),
+      )
+  }
+
+  checkAuthentication():Observable<boolean> | boolean{
+    if(!localStorage.getItem('token')) return false;
+    const token = localStorage.getItem('token');
+    return this.httpClient.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        map(user => !!user),
+        catchError(err => of(false))
+      );
+  }
+
+  logout(){
+    this.user = undefined;
+    localStorage.clear();
   }
 }
